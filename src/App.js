09f99fdff1f6cc34b18a78eea206ad2132032bed
mzaouchi <mzaouchi@gmail.com>
2024-12-23 12:46:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ListUsers from './Components/ListUsers';
+import NavUsers from './Components/NavUsers';
+
+function App() {
+  return (
+    <div>
+      <NavUsers/>
+      <ListUsers/>
+    </div>
+  );
+}
+
+export default App;
